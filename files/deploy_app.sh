39db238583264990c0be791d4465e9491f2dedfa
zehringjohn <john.zehring@navient.com>
2019-10-09 16:56:23 +0000

    Update website.

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -9,9 +9,9 @@ cat << EOM > /var/www/html/index.html
   <div style="width:800px;margin: 0 auto">
 
   <!-- BEGIN -->
-  <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
-  <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own. 
+  <center><img src="http://john-meow.centralus.cloudapp.azure.com/"></img></center>
+  <center><h2>Meow World</h2></center>
+  Welcome to ${PREFIX}'s app. test. 
   <!-- END -->
   
   </div>
